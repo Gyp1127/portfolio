0fa7efcf702b874e13c76883f0d2b1e3470e003a
Gyp1127 <yupin.gan@gmail.com>
2024-03-04 23:41:31 +0800

    fixed load in transition & added summary and CTA buttons

--- app/temp/page.tsx
+++
@@ -1,1 +0,0 @@
-// write a new page using this format
--- components/intro.tsx
+++ components/intro.tsx
@@ -4,10 +4,14 @@ import Image from 'next/image';
 import React from 'react';
 import profilePic from '@/public/yp image.jpg';
 import { motion } from 'framer-motion';
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi"
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
   return (
-    <section>
+    <section className='mb-27 max-w-[50rem] text-center sm:mb-0'>
         <div className="flex items-center justify-center">
             <div className='relative'>
                 <motion.div
@@ -31,6 +35,41 @@ export default function Intro() {
                 >✌</motion.span>
             </div>
         </div>
+
+        <motion.p 
+            className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text4xl'
+            initial={{opacity: 0, y: 100}}
+            animate={{opacity:1, y:0}}
+            >
+            <span className='font-bold'>Lorem ipsum dolor sit</span>, amet consectetur adipisicing elit.{" "}
+            <span className='font-bold'>Ipsum labore</span> necessitatibus id veritatis doloribus sint voluptatem rerum omnis, 
+            aliquid ab doloremque recusandae nesciunt, consequatur reprehenderit odio dolore culpa inventore modi dignissimos eveniet nihil facilis et.
+        </motion.p>
+
+        <motion.div 
+            className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+            initial={{opacity:0, y:100}}
+            animate={{opacity:1, y:0}}
+            transition={{delay:0.1}}>
+            <Link 
+                href="#contact"
+                className='group bg-stone-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-stone-950 active:scale-105 transition cursor-pointer border border-black/10'>
+                    Contact Me <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
+            </Link>
+            <a
+                className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10' 
+                href="/CV.pdf"
+                download>
+                    Download CV <HiDownload className="opacity-60 group-hover:translate-x-1 transition"/>
+            </a>
+            <a className="bg-white p-4 text-stone-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-stone-950 active:scale-105 transition cursor-pointer border border-black/10" 
+            href="https://www.linkedin.com/in/ganyupin/"
+            target="_blank"><BsLinkedin /></a>
+            <a className="bg-white p-4 text-stone-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-stone-950 active:scale-105 transition cursor-pointer border border-black/10" 
+            href="https://github.com/Gyp1127"
+            target="_blank"><FaGithubSquare /></a>
+
+        </motion.div>
     </section>
   )
 }
