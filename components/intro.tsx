346f8d19b904d328d5bb4c6664bdf71419b0eb57
Gyp1127 <yupin.gan@gmail.com>
2024-03-05 16:28:21 +0800

    project section (examples; to be amended later)

--- components/intro.tsx
+++ components/intro.tsx
@@ -21,8 +21,6 @@ export default function Intro() {
                 >
                     <Image src={profilePic}
                     alt="Profile image"
-                    width="192"
-                    height="192"
                     quality="95"
                     priority={true}
                     className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl" />
--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -5,7 +5,7 @@ type SectionHeadingProps = {children: React.ReactNode}
 export default function SectionHeading({children}: SectionHeadingProps) {
   return (
     <h2
-    className='text-3xl font-medium capitalize mb-8'
+    className='text-3xl font-medium capitalize mb-8 text-center'
     >{children}</h2>
   )
 }
