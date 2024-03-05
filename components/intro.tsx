aee60a3edea3703df54b8f88516f7931fb2f83c8
Gyp1127 <yupin.gan@gmail.com>
2024-03-05 18:07:52 +0800

    added moving shadow in nav bar using hooks

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,10 +8,14 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi"
 import { FaGithubSquare } from 'react-icons/fa';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Intro() {
+
+  const { ref } = useSectionInView("Home", 0.5)
+
   return (
-    <section className='mb-27 max-w-[50rem] text-center sm:mb-0'>
+    <section id="home" className='mb-27 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]' ref={ref}>
         <div className="flex items-center justify-center">
             <div className='relative'>
                 <motion.div
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+"use client"
+
+import React  from 'react'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data'
 import Project from './project'
+import { useSectionInView } from '@/lib/hooks'
 
 
 
 export default function Projects() {
+
+  const { ref } = useSectionInView("Projects", 0.5)
+
   return (
-    <section>
+    <section id="projects" className="scroll-mt-28" ref={ref}>
         <SectionHeading>
             My Projects
         </SectionHeading>
