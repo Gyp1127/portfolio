f940b9335978b9f21248044b3b0c4b412a0988c2
Gyp1127 <yupin.gan@gmail.com>
2024-03-05 23:16:32 +0800

    fixed &apos; in about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -22,8 +22,8 @@ export default function About() {
         </SectionHeading>
 
         <p className="mb-3">Having attended my first hackathon in University, I continued on to host a range of accessible educational and competitive events catering to students interested in learning about the world of tech and seasoned hackers looking for a challenge. I also lent my expertise to other clubs to help them run smoother and more impactful events. </p>
-        <p>My time as the President of HackMelbourne and Treasurer of Women In Technology at the University of Melbourne allowed me to shape the landscape of events on Campus, proving that no event is too small to make a difference in people's academic and social life</p>
-        
+        <p>My time as the President of HackMelbourne and Treasurer of Women In Technology at the University of Melbourne allowed me to shape the landscape of events on Campus, proving that no event is too small to make a difference in people&apos;s academic and social life</p>
+
     </motion.section>
   )
 }
